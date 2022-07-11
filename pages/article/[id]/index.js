eb8e22d4f7d722fe881efe39426e3dec3f6d401e
a.urbanite <a.urbanite@gmail.com>
2022-07-11 18:47:00 +0200

    implemented alternative way to extract article id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,10 @@
+// import useRouter from 'next/router'
 import Link from "next/link"
 
 const ArticlePage = ({article}) => {
+  // const router = useRouter();
+  // const { id } = router.query
+
   return (
     <>
       <div>This is article {article.id}</div>
@@ -16,7 +20,7 @@ const ArticlePage = ({article}) => {
 export default ArticlePage
 
 export const getServerSideProps = async (context) => {
-  
+
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
   const article = await res.json()
 
